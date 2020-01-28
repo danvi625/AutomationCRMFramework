1d7688cef811c2e59de105ce5149dfa65b32be4a
green <green@green-PC>
2020-01-28 20:28:03 +0530

    first commit

---
+++ AutomationCRMFramework/src/com/crm/genericlib/BaseClass.java
@@ -0,0 +1,12 @@
+package com.crm.genericlib;
+
+import org.testng.annotations.Test;
+
+public class BaseClass {
+
+	@Test
+	public void configBS()
+	{
+		System.out.println("from BS");
+	}
+}
---
+++ AutomationCRMFramework/src/com/crm/scripts/OrgTest.java
@@ -0,0 +1,13 @@
+package com.crm.scripts;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	public void createOrg()
+	{
+		//step 1
+		//step 2
+	}
+}
